e5e75d1e12a46cf712b052c5880e6ec7119d6a42
mrbeonghia <ntnghia199x@gmail.com>
2021-03-11 15:56:19 +0700

    aop and exception

---
+++ src/main/java/com/codegym/cms/config/AppInit.java
@@ -0,0 +1,33 @@
+package com.codegym.cms.config;
+
+import com.codegym.cms.config.AppConfig;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletRegistration;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+        DispatcherServlet dispatcher = (DispatcherServlet) super.createDispatcherServlet(servletAppContext);
+        dispatcher.setThrowExceptionIfNoHandlerFound(true);
+        return dispatcher;
+    }
+}
